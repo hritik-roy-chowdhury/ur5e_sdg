e0641a9ea5fb6444f225c94cce37af10c60d1e32
Ubuntu <ubuntu@ip-172-31-24-69.ec2.internal>
2025-07-16 13:44:30 +0000

    Changed image dimensions to 1200x900

--- generate_data.sh
+++ generate_data.sh
@@ -14,6 +14,6 @@ cd $ISAAC_SIM_PATH
 
 echo "Starting Data Generation"  
 
-./python.sh $SCRIPT_PATH --height 480 --width 640 --num_frames 1000 --data_dir $OUTPUT_DATA
+./python.sh $SCRIPT_PATH --height 900 --width 1200 --num_frames 1000 --data_dir $OUTPUT_DATA
 
 
